470d90c2d351fb2d260c53c9edda0c4b6c068a8f
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-02-14 13:11:25 +0500

    other edits

---
+++ main.js
@@ -0,0 +1,21 @@
+const http = require("http")
+const url = require("url")
+const fs = require("fs")
+const CreateList = require("./posts")
+
+const Data  = fs.readFileSync("./back.json", "utf-8", (err, data) => {
+    if(err) throw err
+    return data
+})
+
+const Home = fs.readFileSync("./index.html", "utf-8", (err,data) =>{
+    if(err) throw err
+    return data
+})
+const a = JSON.parse(Data)
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write(CreateList(a))
+    res.end(Home)
+}).listen(1111)
--- posts.js
+++ posts.js
@@ -1,3 +1,3 @@
 module.exports = function CreateList(data) {
-    return `<ul>${data.map(elem => {`<li><h1>${elem.title}</h1><p>${elem.body}</p></li>`})}</ul>`
+    return `<ul>${data.map(elem => {`<li><h2>${elem.title}</h2><p>${elem.body}</p></li>`})}</ul>`
 }
